01ee79a383c7e99c73547418021cd98ed496c079
diegovbmachado <diegobardo@hotmail.com>
2023-05-30 17:38:44 -0300

    finalizando validacao
    validacao sem banco de dados

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function validateFields() {
-  const emailValid = isEmailValid();
-  document.getElementById("recover-password-button").disabled = !emailValid;
-
-  const passwordValid = isPasswordValid();
-  document.getElementById("loguin-button").disabled =
-    !emailValid || !passwordValid;
+  toggleEmaiErrors();
+  toggleButtonsDisable();
+  togglePasswordErrors();
   // const email = document.getElementById("email").value;
   // if (!email) {
   //   document.getElementById("recover-password-button").disabled = true;
@@ -21,6 +18,38 @@ function isEmailValid() {
   }
   return validateEmail(email);
 }
+function toggleEmaiErrors() {
+  const email = document.getElementById("email").value;
+  if (!email) {
+    document.getElementById("email-required-error").style.display = "block";
+  } else {
+    document.getElementById("email-required-error").style.display = "none";
+  }
+  if (validateEmail(email)) {
+    document.getElementById("email-invalid-error").style.display = "none";
+  } else {
+    document.getElementById("email-invalid-error").style.display = "block";
+  }
+}
+
+function togglePasswordErrors(){
+    const password = document.getElementById("password").value;
+    if(!password){
+        document.getElementById("password-required-error").style.display = "block";  
+    }else {
+        document.getElementById("password-required-error").style.display = "none"; 
+    }
+
+}
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  document.getElementById("recover-password-button").disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  document.getElementById("loguin-button").disabled =
+    !emailValid || !passwordValid;
+}
+
 function isPasswordValid() {
   const password = document.getElementById("password").value;
   if (!password) {
