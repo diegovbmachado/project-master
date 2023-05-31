437825ddc44e7420fef3cacf63078bc3893846ad
diegovbmachado <diegobardo@hotmail.com>
2023-05-31 17:28:24 -0300

    lapidando codigo
    redução nos codigos apenas

--- index.js
+++ index.js
@@ -6,60 +6,67 @@ function onChangePassword() {
   togglePasswordErrors();
   toggleButtonsDisable();
 }
-// const email = document.getElementById("email").value;
-// if (!email) {
-//   document.getElementById("recover-password-button").disabled = true;
-// } else if (validateEmail(email)) {
-//   document.getElementById("recover-password-button").disabled = false;
-// } else if (validateEmail(email)) {
-//   document.getElementById("recover-password-button").disabled = true;
-// }
 
 function isEmailValid() {
-  const email = document.getElementById("email").value;
+  const email = from.email().value;
   if (!email) {
     return false;
   }
   return validateEmail(email);
 }
 function toggleEmaiErrors() {
-  const email = document.getElementById("email").value;
-  if (!email) {
-    document.getElementById("email-required-error").style.display = "block";
-  } else {
-    document.getElementById("email-required-error").style.display = "none";
-  }
-  if (validateEmail(email)) {
-    document.getElementById("email-invalid-error").style.display = "none";
-  } else {
-    document.getElementById("email-invalid-error").style.display = "block";
-  }
+  const email = from.email().value;
+  from.emailRequiredError().style.display = email ? "none" : "block";
+  //oque esta acima, faz exatamente oque esta a baixo, genial!
+  // if (!email) {
+  //   from.emailRequiredError().style.display = "block";
+  // } else {
+  //   from.emailRequiredError().style.display = "none";
+  // }
+  from.emailInvalidError().style.displa = validateEmail(email)
+    ? "none"
+    : "block";
+  //oque esta acima, faz exatamente oque esta a baixo, genial!
+  // if (validateEmail(email)) {
+  //   from.emailInvalidError().style.display = "none";
+  // } else {
+  //   from.emailInvalidError().style.display = "block";
+  // }
 }
 
 function togglePasswordErrors() {
-  const password = document.getElementById("password").value;
-  if (!password) {
-    document.getElementById("password-required-error").style.display = "block";
-  } else {
-    document.getElementById("password-required-error").style.display = "none";
-  }
+  const password = from.password().value;
+
+  from.passwordRequiredError().style.display = password ? "none" : "block";
+  // if (!password) {
+  //   from.passwordRequiredError().style.display = "block";
+  // } else {
+  //   from.passwordRequiredError().style.display = "none";
+  // }
 }
 function toggleButtonsDisable() {
   const emailValid = isEmailValid();
-  document.getElementById("recover-password-button").disabled = !emailValid;
+  from.recoverPassword().disabled = !emailValid;
 
   const passwordValid = isPasswordValid();
-  document.getElementById("loguin-button").disabled =
-    !emailValid || !passwordValid;
+  from.loguinButton().disabled = !emailValid || !passwordValid;
 }
 
 function isPasswordValid() {
-  const password = document.getElementById("password").value;
+  const password = from.password().value;
   if (!password) {
     return false;
   }
   return true;
 }
-function validateEmail(email) {
-  return /\S+@\S+\.\S+/.test(email);
-}
+
+const from = {
+  email: () => document.getElementById("email"),
+  password: () => document.getElementById("password"),
+  passwordRequiredError: () =>
+    document.getElementById("password-required-error"),
+  loguinButton: () => document.getElementById("loguin-button"),
+  recoverPassword: () => document.getElementById("recover-password-button"),
+  emailInvalidError: () => document.getElementById("email-invalid-error"),
+  emailRequiredError: () => document.getElementById("email-required-error"),
+};
