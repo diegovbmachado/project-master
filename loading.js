413fa7379ebbd0ee278cb82b594f611e10b60081
diegovbmachado <diegobardo@hotmail.com>
2023-06-02 19:06:55 -0300

    agora foi

--- loading.js
+++ loading.js
@@ -1,19 +1,18 @@
 function showLoading() {
   const div = document.createElement("div");
   div.classList.add("loading", "centralize");
-  document.body.appendChild(div);
 
   const label = document.createElement("label");
   label.innerText = "Carregango...";
   div.appendChild(label);
-  document.body.appendChild(label);
+  document.body.appendChild(div);
 
   setTimeout(() => hideLoading(), 2000);
 }
 
 function hideLoading() {
-  const loading = document.getElementsByClassName("loading");
-  if (loading.length) {
-    loading[0].remove();
+  const loadings = document.getElementsByClassName("loading");
+  if (loadings.length) {
+    loadings[0].remove();
   }
 }
