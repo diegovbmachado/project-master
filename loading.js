4beb6f510c13c0af8d974f996e1afe084bae1025
diegovbmachado <diegobardo@hotmail.com>
2023-06-02 17:39:44 -0300

    teste error

--- loading.js
+++ loading.js
@@ -1,5 +1,19 @@
 function showLoading() {
-  alert("loading...");
+  const div = document.createElement("div");
+  div.classList.add("loading", "centralize");
+  document.body.appendChild(div);
+
+  const label = document.createElement("label");
+  label.innerText = "Carregango...";
+  div.appendChild(label);
+  document.body.appendChild(label);
+
+  setTimeout(() => hideLoading(), 2000);
 }
 
-function hideLoading() {}
+function hideLoading() {
+  const loading = document.getElementsByClassName("loading");
+  if (loading.length) {
+    loading[0].remove();
+  }
+}
