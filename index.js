7a814e87b9f9142938b8f63e76b11e091d59a5ee
diegovbmachado <diegobardo@hotmail.com>
2023-06-02 16:59:40 -0300

    carregando
    colocando um loading basicamente...

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function getErrorMessage(error) {
 }
 
 function register() {
-  window.location.href = "pages/register/register.html";
+  showLoading();
+  // window.location.href = "pages/register/register.html";
 }
 
 function isEmailValid() {
---
+++ loading.js
@@ -0,0 +1,5 @@
+function showLoading() {
+  alert("loading...");
+}
+
+function hideLoading() {}
