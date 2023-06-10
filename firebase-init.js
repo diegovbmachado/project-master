72997409c487e16e4e5cfda082eeb9f4e377be40
diegovbmachado <diegobardo@hotmail.com>
2023-06-10 17:48:26 -0300

    firebase-init.js
    colocando tudo do firebase num novo arquivo e chamando as novas extensões

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7kZeiiaOWR_iTEQXNG8WNHa4e3YNBSjk",
+  authDomain: "controle-de-gastos-de472.firebaseapp.com",
+  projectId: "controle-de-gastos-de472",
+  storageBucket: "controle-de-gastos-de472.appspot.com",
+  messagingSenderId: "1049997118098",
+  appId: "1:1049997118098:web:0f4245c9dc4019450b344b",
+};
+firebase.initializeApp(firebaseConfig);
